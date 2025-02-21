60367e8ef9700fa978998c606baeea7a484b36c7
Kutalia <kotekutalia@gmail.com>
2025-02-21 15:49:22 +0400

    avoid saving inactive visitor data

--- src/middleware.ts
+++ src/middleware.ts
@@ -16,19 +16,19 @@ mutation RegisterVisitor {
 `)
 
 export async function middleware(request: NextRequest) {
-  // Creating session cookie if there's no visitor token used for authentication
+  // Creating session cookie if there's no visitor data used for authentication
   const response = NextResponse.next()
 
-  if (!request.cookies.has('visitorToken')) {
+  if (!request.cookies.has('visitor')) {
     const result = await apolloClient
       .mutate({
         mutation: REGISTER_VISITOR,
         fetchPolicy: 'no-cache',
       })
 
-    const token = result.data?.register?.token
-    if (token) {
-      response.cookies.set('visitorToken', token)
+    const visitor = result.data?.register
+    if (visitor && visitor.isActive) {
+      response.cookies.set('visitor', JSON.stringify(visitor))
     }
   }
 
